0ee047e6a8cb58ee2d8eea7fd630b3a8919c3b8d
andi Klein <pabloemma@gmail.com>
2022-08-03 14:42:22 -0600

    modified the read json program such that now the whole AP output goes into a Pandas data frame and we can now print out csv files.

--- RRD_SNMP/src/JsonRead.py
+++ RRD_SNMP/src/JsonRead.py
@@ -4,6 +4,8 @@ Created on Aug 19, 2020
 @author: klein
 '''
 import json
+import pandas as PD
+from collections.abc import MutableMapping
 
 
 class JsonRead(object):
@@ -16,6 +18,11 @@ class JsonRead(object):
         '''
         Constructor
         '''
+        self.AllData = []  # has all the data in it
+
+        #remove limits on columns and rows
+        PD.set_option('display.max_rows', 10)
+        PD.set_option('display.max_columns', 500)
         
     def ReadFile(self,filename):
         """
@@ -26,7 +33,7 @@ class JsonRead(object):
             if isinstance(self.data,list):
                 for k in range(0,len(self.data)):
                     self.IterDict(self.data[k])
-                    print('\n\n')
+                    print('\n ******************************* \n')
             else:
                 self.IterDict(self.data)
                 
@@ -36,32 +43,66 @@ class JsonRead(object):
         if isinstance(self.data,list):
             for k in range(0,len(self.data)):
                 self.IterDict(self.data[k])
-                print('\n\n')
+                
+                print('\n ***************************\n')
             else:
                 self.IterDict(self.data)
   
     def IterDict(self,dictionary):
         """ Iterates through nested dictionaries
         """
+        flat_dict = {}   # the flattened dictionary
         if isinstance(dictionary,dict):
         
             for k,v in dictionary.items():
                 if isinstance(v,dict):
                 # we have a nested dictionary
                     self.IterDict(v)
+                    print(" inner dict",v)
                 else:
                     if(v != None):
                         print(k, ":",v)
+                    else:
+                        print(k ,": None")
         else:
-            " we have a list"
+            print(" we have a list")
             
             self.IterDict(dictionary[0])
-            
-                
-            
+
+    
+    def CreatePandas(self):
+
+        """ converts dictionaries into panda data structure
+            where the data have been read into self.data
+        """
+        # loop over all the data and create a list of dictionaries
+        for k in range(0,len(self.data)):
+            flat_dict = self.FlatMyDict(self.data[k])
+            self.AllData.append(flat_dict)
+        # Now we have all in a list, we can create a large Pandas frame
+        self.AllDataFrame = PD.DataFrame(self.AllData)
+        print(self.AllDataFrame)
+
+    
+
+        # print(self.AllData)
+
+    def Pandas2CSV(self,filename):
+        """converts pandas into csv file"""
+        self.AllDataFrame.to_csv(filename)
+        return
+
+    def FlatMyDict(self,data: MutableMapping, sep: str= '.') -> MutableMapping:
+        """ flattens the dictionary into one"""  
+        [flat_dict] = PD.json_normalize(data, sep=sep).to_dict(orient='records')
+        return flat_dict
+      
 
 if __name__ == '__main__':
-    filename='/Users/klein/scratch/respo.json'
+    filename='/home/klein/scratch/AP.json'
     JR = JsonRead()
     JR.ReadFile(filename)
+    JR.CreatePandas()
+    JR.Pandas2CSV('/home/klein/scratch/AP.csv')
+    #JR.FlatMyDict()
     
